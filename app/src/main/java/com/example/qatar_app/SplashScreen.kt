b10c41b50d2c9685be61d6bc85eed4e98ad0bcd6
mejdi <mejdiclubiste@gmail.com>
2020-11-15 22:19:59 +0100

    creating the winner app

---
+++ app/src/main/java/com/example/qatar_app/SplashScreen.kt
@@ -0,0 +1,40 @@
+package com.example.qatar_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import androidx.constraintlayout.motion.widget.MotionLayout
+import kotlinx.android.synthetic.main.activity_splash_screen.*
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed({
+            startAnimation()
+        }, 4000)
+    }
+
+    private fun startAnimation() {
+        motion.transitionToState(R.id.collapsed)
+        motion.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+                Log.d("nice", "onTransitionStarted: ")
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
+                Log.d("nice", "onTransitionStarted: ")
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
+                Log.d("nice", "onTransitionStarted: ")
+            }
+
+        })
+    }
+}
